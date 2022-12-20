5179b1636d5d2539f746881e64416e3af18bd699
Denis Davydov <ivnssnn@gmail.com>
2022-12-20 11:40:35 +0300

    feat(ui): setup ui

---
+++ ui/src/components/molecules/ThemeProvider/index.tsx
@@ -0,0 +1,17 @@
+import { CssBaseline, StyledEngineProvider } from "@mui/material";
+import { ReactElement, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function ThemeProvider(props: Props): ReactElement {
+  const { children } = props;
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <CssBaseline />
+      {children}
+    </StyledEngineProvider>
+  );
+}
---
+++ ui/src/components/templates/Main/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function MainTemplate(props: Props): ReactElement {
+  const { children } = props;
+
+  return <>{children}</>;
+}
