f86bac8d9bae513577da7043b4facc8419a9a5bd
Denis Davydov <89587396+DenisDavydoB@users.noreply.github.com>
2023-01-05 20:13:26 +0300

    feat(ui): add effectorjs (#5)
    Co-authored-by: Denis Davydov <ivnssnn@gmail.com>

--- ui/src/components/molecules/FlippyCard/index.tsx
+++ ui/src/components/molecules/FlippyCard/index.tsx
@@ -1,25 +1,21 @@
 import { SxProps } from '@mui/material'
-import { ReactElement, ReactNode, useState } from 'react'
+import { ReactElement, ReactNode } from 'react'
 import ReactCardFlip from 'react-card-flip'
 import { Container } from './styles.js'
 
 interface Props {
   front: ReactNode
   back: ReactNode
+  isFlipped: boolean
   sx: SxProps
+  onClick: () => void
 }
 
 export default function FlippyCard(props: Props): ReactElement {
-  const { back, front, ...rest } = props
-
-  const [isFlipped, setIsFlipped] = useState(false)
-
-  const handleClick = () => {
-    setIsFlipped((prev) => !prev)
-  }
+  const { back, front, isFlipped, onClick, ...rest } = props
 
   return (
-    <Container {...rest} onClick={handleClick}>
+    <Container {...rest} onClick={onClick}>
       <ReactCardFlip isFlipped={isFlipped}>
         {front}
         {back}
