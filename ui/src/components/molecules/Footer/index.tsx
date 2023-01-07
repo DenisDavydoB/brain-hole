605dfdaa5ffb9b8fa84de6c691c4bf3fc02cf235
Denis Davydov <89587396+DenisDavydoB@users.noreply.github.com>
2023-01-07 12:40:49 +0300

    fefactor(ui): main page layout (#10)
    Co-authored-by: Denis Davydov <ivnssnn@gmail.com>

--- ui/src/components/molecules/Footer/index.tsx
+++ ui/src/components/molecules/Footer/index.tsx
@@ -1,10 +1,14 @@
-import { Stack, Typography } from '@mui/material'
+import { Stack } from '@mui/material'
 import { lightBlue } from '@mui/material/colors'
 
 export default function Footer() {
   return (
-    <Stack alignItems="center" direction="row" height="40px" sx={{ backgroundColor: lightBlue[900] }} width="100%">
-      <Typography>Footer</Typography>
-    </Stack>
+    <Stack
+      alignItems="center"
+      direction="row"
+      height="40px"
+      sx={{ backgroundColor: lightBlue[900] }}
+      width="100%"
+    ></Stack>
   )
 }
