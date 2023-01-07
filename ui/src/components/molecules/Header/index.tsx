b75b84a4cc0f6a19450add0cee14db85a4191eed
Denis Davydov <89587396+DenisDavydoB@users.noreply.github.com>
2023-01-07 22:50:48 +0300

    feat(ui): add timer indicator (#13)
    Co-authored-by: Denis Davydov <ivnssnn@gmail.com>

--- ui/src/components/molecules/Header/index.tsx
+++ ui/src/components/molecules/Header/index.tsx
@@ -1,11 +1,19 @@
 import { Avatar, Stack, Typography } from '@mui/material'
+import Timer from './components/Timer/index.js'
 
 export default function Header() {
   return (
-    <Stack alignItems="center" direction="row" justifyContent="space-between" sx={{ pl: 4, pr: 4 }} width="100%">
+    <Stack
+      alignItems="center"
+      direction="row"
+      justifyContent="space-between"
+      sx={{ pl: 4, pr: 4, position: 'relative' }}
+      width="100%"
+    >
       <Typography color={'ButtonFace'} sx={{ m: 2 }} variant="h5">
         Find a match
       </Typography>
+      <Timer />
       <Avatar sx={{ m: 2 }}>OP</Avatar>
     </Stack>
   )
