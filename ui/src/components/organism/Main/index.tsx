3fdbdb5710706207b106a3b33cb1eff7b676e791
Denis Davydov <ivnssnn@gmail.com>
2023-01-15 11:20:41 +0300

    feat(ui): add loading indicator

--- ui/src/components/organism/Main/index.tsx
+++ ui/src/components/organism/Main/index.tsx
@@ -1,12 +1,13 @@
 import { Stack } from '@mui/material'
 import CardsList from './components/CardsList/index.js'
+import Loading from './components/Loading/index.js'
 import Start from './components/Start/index.js'
 import Stats from './components/Stats/index.js'
 import { useApplicationState } from '@/model/hooks.js'
 import { applicationStart } from '@/model/index.js'
 
 export default function Main() {
-  const { done, idle } = useApplicationState()
+  const { done, idle, loading } = useApplicationState()
 
   const handleClick = () => {
     applicationStart()
@@ -16,6 +17,7 @@ export default function Main() {
     <Stack alignItems="center" justifyContent={'center'} spacing={1} sx={{ p: 2, height: '100%', width: '100%' }}>
       <Stack direction={'column'} gap={2} sx={{ position: 'relative', width: 'fit-content' }}>
         <CardsList />
+        {loading ? <Loading /> : null}
         {idle ? <Start onClick={handleClick} /> : null}
         {done ? <Stats onClick={handleClick} /> : null}
       </Stack>
