4d0eeec3134003b88974788eadbdf8dc3360433d
Denis Davydov <ivnssnn@gmail.com>
2023-01-22 22:00:26 +0300

    fix(ui): mobile scroll

--- ui/src/components/organism/Main/components/Card/index.tsx
+++ ui/src/components/organism/Main/components/Card/index.tsx
@@ -1,10 +1,12 @@
 import { Grid } from '@mui/material'
-import { memo, ReactElement } from 'react'
+import { CSSProperties, memo, ReactElement } from 'react'
 import FlippyCard from '@/components/molecules/FlippyCard/index.js'
 import { openCard } from '@/model/index.js'
 import type { CardItem } from '@/model/types.js'
 
 const CARD_COVER_URL = './other/cover.png'
+
+const style: CSSProperties = { maxWidth: '100%', minWidth: 0, maxHeight: '100%', minHeight: 0 }
 
 interface Props {
   card: CardItem
@@ -21,8 +23,8 @@ export const Card = memo(function Card(props: Props): ReactElement {
 
   const opacity = isGuessed ? '0' : '1'
 
-  const cardFront = <img height="100%" src={CARD_COVER_URL} width="100%" />
-  const cardBack = <img height="100%" src={card.url} width="100%" />
+  const cardFront = <img height={100} src={CARD_COVER_URL} style={style} width={100} />
+  const cardBack = <img height={100} src={card.url} style={style} width={100} />
 
   return (
     <Grid item sm={2} sx={{ opacity, transition: 'opacity 750ms' }} xs={3}>
@@ -30,7 +32,7 @@ export const Card = memo(function Card(props: Props): ReactElement {
         back={cardBack}
         front={cardFront}
         isFlipped={isOpened}
-        sx={{ borderRadius: '5%' }}
+        sx={{ borderRadius: '5%', aspectRatio: '1/1', overflow: 'hidden' }}
         onClick={handleClick}
       />
     </Grid>
